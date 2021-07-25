02b5dda9b744d9785006d56eba7d140af8183c64
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-24 20:57:55 -0600

    Add class component for App

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/styles.scss'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  render() {
+    return (
+      <h1>onlyDevs</h1>
+    )
+  }
+}
+
+export default App;
