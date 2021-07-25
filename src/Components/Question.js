c6bde8ec3437c833650c41dc32e75728dfbf400a
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-25 18:55:03 -0400

    Add Question component and build basic structure of the return

---
+++ src/Components/Question.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink, Route } from 'react-router-dom';
+
+const Question = () => {
+
+    //const randomQuestion = calculate random question
+
+    return (
+        <article className= 'questionBox'>
+            <div>
+                <h2>{randomQuestion}</h2>
+            </div>
+            <form className= 'answerInput'>
+                {/*<Answer /> //I think to do this correctly we should have this be a component that will hold state... it will also contain the submit button to set the value to POST and thus re-render App...? */}
+            </form>
+        </article>
+    )
+}
+
+export default Question;
