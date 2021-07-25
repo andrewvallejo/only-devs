a5a5b0b2893a4978c72b422705eec65ad01e202c
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-25 16:46:48 -0400

    Update path for Header component in App

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import Header from ../Header;
+import Header from './Header';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {}
   }
+
   render() {
     return (
       <nav>
