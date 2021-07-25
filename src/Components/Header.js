7ed643a81d2a805758968cdedf8376cdd51ae27c
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-25 16:34:42 -0400

    Add header element and NavLinks to Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import { Route, NavLink, Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <nav className='nav'>
+            <header>
+                <h1>OnlyDevs</h1>
+            </header>
+            <div className='navBtns'>
+                <NavLink to="/all-questions" className='ViewAll'>
+                    <button>View All Questions</button>
+                </NavLink> 
+                <button className='</button>
+            </div>
+        </nav>
+    )
+}
