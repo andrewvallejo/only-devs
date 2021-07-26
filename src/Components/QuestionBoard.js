e1776852f1e83acf1a9322132ba2b5ec3343bb74
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-25 21:17:06 -0400

    Build QuestionBoard component with question Cards

---
+++ src/Components/QuestionBoard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const QuestionBoard = () => {
+    
+    //const questionCards = map through all questions 
+    //and return (
+        //<Card 
+        //  id, key, question, answers.length
+        // />
+    // )
+    
+//     return (
+//         <section className='cardsContainer'>{questionCards}</section>
+//     )
+// }
+
+export default QuestionBoard;
