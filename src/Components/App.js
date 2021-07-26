ce6e4b5ea393d3af352cc1dbb04244d81ac253e8
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-25 21:31:42 -0400

    Pseudocode render for Question and QuestionBoard

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Header from './Header';
 import Question from './Question'
+import QuestionBoard from './QuestionBoard'
 
 class App extends Component {
   constructor() {
@@ -17,6 +18,7 @@ class App extends Component {
         <Route exact path = '/' render={() => 
           <Question />
           }/>
+        {/* <QuestionBoard /> this will be in place of lines 18-20 and will hold all of our view options based on the route path */}
         </section>
       </main>
     )
