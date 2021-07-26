514ebcbe3753cc83bdf8f608e1699240f6a8480a
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-26 17:21:42 -0400

    Display a random question upon launch.  Pass question props to Answer component

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -3,19 +3,26 @@ import React from 'react';
 
 const Question = ({questions}) => {
 
-    const allQuestions = questions.map(questionObj => questionObj.question)
-    console.log(allQuestions);
+    const randomQuestionObj = questions[Math.floor(Math.random() * questions.length)];
+    console.log(randomQuestionObj)
+    const { id, question, answers } = randomQuestionObj
     // const randomQuestion = calculate random question here
     return (
         <article className= 'questionBox'>
             <div>
-                <h2>Insert Random Question Here</h2>
+                <h2>{question}</h2>
             </div>
             <form className= 'answerInput'>
-                {/*<Answer /> //I think to do this correctly we should have this be a component that will hold state... it will also contain the submit button to set the value to POST and thus re-render App...? */}
+                <Answer 
+                question={question}
+                id={id}
+                answers={answers}
+                /> 
             </form>
         </article>
     )
 }
+
+//I think to do this correctly we should have this be a component that will hold state... it will also contain the submit button to set the value to POST and thus re-render App...? */}
 
 export default Question;
