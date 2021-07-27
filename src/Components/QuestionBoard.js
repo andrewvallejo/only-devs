5d64509ddc9d1d7b07c278c5fea0f069b9b23331
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-27 11:56:52 -0600

    Update questionObj to be more semantic

--- src/Components/QuestionBoard.js
+++ src/Components/QuestionBoard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import QuestionCard from './QuestionCard';
 
 const QuestionBoard = ({questions}) => {
-    const questionCards = questions.map(questionObj => {
+    const questionCards = questions.map(question => {
         return (
             <QuestionCard 
-                id={questionObj.id}
-                key={questionObj.id}
-                question={questionObj.question}
-                answers={questionObj.answers}
+                id={question.id}
+                key={question.id}
+                question={question.question}
+                answers={question.answers}
             />
         )
     });
