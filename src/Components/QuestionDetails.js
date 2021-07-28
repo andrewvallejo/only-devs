617d8b0d75566a8b689b88e5133b0831dae429ee
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-28 14:17:47 -0400

    Add map method for mapping each answer to a div to display on the page

--- src/Components/QuestionDetails.js
+++ src/Components/QuestionDetails.js
@@ -2,7 +2,34 @@ import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
 
 
-const QuestionDetails = () => {
+const QuestionDetails = ({id, questions, answers}) => {
+
+    const displayedQuestion = questions.find(question => question.id === id)
+    
+    //we need to somehow pass in props JUST the answers for that question id... map through them to create a separate
+    //div for each one. then we can insert them in our return in line 25 (not the correct layout, but just giving you the general idea)
+    const allAnswers = answers.map(answer => {
+        return (
+            <article className='answer'>
+                <div className='time-rating'>
+                    <p>{answer.time_stamp}</p>
+                    <button className='rating'>{answer.rating} likes</button>
+                </div>
+                <p>{answer.answer}</p>
+            </article>
+        )     
+    })
+
+    //this will be the actual larger details "box" being displayed... with inserted answers based on question.id
+    
+    return (
+        <section className='details-container'>
+            <div>
+                <h3>{displayedQuestion.question}</h3>
+            </div>
+            <section className='all-answers'>{allAnswers}</section>
+        </section>
+    )
 
 
 
