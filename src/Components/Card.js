1175207eb4214a900a816d0a808b6b648247b75d
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-29 12:37:32 -0400

    Update class naming

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Card = ({ question, id, answers }) => {
     return (
         <NavLink to={`/question-details-${id}`}>
-            <article className='questionCard' id={id}>
+            <article className='question-card' id={id}>
                 <h3>{question}</h3>
                 <p>{answers.length} of Answers</p>
                 <button>View Details</button>
