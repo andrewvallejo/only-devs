bc55d3115581e7b32d35b31d2ff718395f21d787
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-29 20:31:20 -0400

    Create fetch for answers

--- src/Utilities/apiCalls.js
+++ src/Utilities/apiCalls.js
@@ -1,13 +1,13 @@
 export const fetchQuestions = () => {
-    return fetch('https://onlydevs-api.herokuapp.com/questions')
-    .then(response => {
-      if (!response.ok) {
-        throw Error()
-      }
-      return response.json()
-    })
+  return fetch('https://onlydevs-api.herokuapp.com/questions')
+  .then(response => {
+    if (!response.ok) {
+      throw Error()
+    }
+    return response.json()
+  })
 }
 
-export const fetchAnswers = () => {
+// export const fetchAnswers = (id) => {
   
-}
+// }
