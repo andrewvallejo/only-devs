47fd7a220e6912e27f51525c90ee803128208b00
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-29 20:00:46 -0600

    Add  submit answer function to update stae

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-export const Question = ({ id, question }) => {
+export const Question = ({ questions, add }) => {
+    const [id, question] = questions
     const [answer, setAnswer] = useState('')
-
     const submitAnswer = (event) => {
         event.preventDefault()
         const newAnswer = {
-            id: Date.now(),
-                
+            id: Date.now(),   
+            answer    
         }
-    }
+        add(newAnswer);
+        setAnswer('') 
+       }
 
     return (
         <form className='question-form'>
