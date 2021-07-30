4851ea3e41893722fc170e51c5c406c2e8da8c3b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-30 16:34:49 -0600

    Add answer component

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -1,14 +1,14 @@
 import React from 'react';
   
  export const Answers = ({answers}) => {
-        return answers.map(answer => {
-            return (
-                <article className='answer' key={answer.id} id={answer.id}>
-                    <p>{answer.answer_time}</p>
-                    <button className='rating'>{answer.rating} likes</button>
-                    <p>{answer.answer}</p>
-                </article>
-            )     
-        })
-    }
+    return answers.map(answer => {
+        return (
+            <article className='answer' key={answer.id} id={answer.id}>
+                <p>{answer.answer_time}</p>
+                <button className='rating'>{answer.rating} likes</button>
+                <p>{answer.answer}</p>
+            </article>
+        )     
+    })
+}
 
