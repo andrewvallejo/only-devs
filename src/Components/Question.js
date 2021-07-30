0cc7e7f75d09f7007b000c72e7dc0a0404113870
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-29 18:58:01 -0600

    Add renturn render for form

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-export const Question = ({ questions }) => {
-    console.log(questions)
-    const [answer, addAnswer] = useState('')
+export const Question = ({ id, question }) => {
+    const [answer, setAnswer] = useState('')
 
     return (
-      
+        <form className='question-form'>
+            <input 
+                id={question.id}
+                type='text'
+                name='answer'
+                placeholder='Write your answer here.'
+                value={answer}
+                onChange={event => setAnswer(event.target.value)}
+            />
+            <NavLink to={`/questions/${id}}`}>
+                <button>SUBMIT</button>
+            </NavLink>
+        </form>
     )
-
-
 }
