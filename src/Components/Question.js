64ee59b384fa000fc0662f5fcb76a9044fe86da9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-29 18:44:22 -0600

    Add Question form skeleton

---
+++ src/Components/Question.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export const Question = ({ questions }) => {
+    console.log(questions)
+    const [answer, addAnswer] = useState('')
+
+    return (
+      
+    )
+
+
+}
