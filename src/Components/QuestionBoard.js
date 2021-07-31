143918f4344f1b7a96fd54f9df12a457c932056f
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-30 18:25:36 -0600

    Add live-search bar

--- src/Components/QuestionBoard.js
+++ src/Components/QuestionBoard.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Card } from './Card';
+import {useState} from 'react';
 
 export const QuestionBoard = ({questions}) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
 
     console.log(questions)
-    const questionCard = questions.map(question => {
+    const questionCard = questions.filter((question) => {
+        if(searchTerm == "") {
+            return question;
+        } else if (question.question.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return question;
+        }
+    }).map(question => {
         return (
             <Card 
                 id={question.id}
@@ -16,7 +25,16 @@ export const QuestionBoard = ({questions}) => {
     });
     
     return (
-        <section className='cards-container'>{questionCard}</section>
+
+        <section className='cards-container'>
+        <input 
+            type="text" 
+            placeholder="Search..." 
+            onChange={(event) => {
+                setSearchTerm(event.target.value);
+                }} />
+        {questionCard}
+        </section>
     )
 }
 
