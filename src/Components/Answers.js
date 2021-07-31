1acaa55fb94abde4eed9e2b52bbf7fb16851d324
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-07-30 20:35:53 -0600

    Add header wrapper for time and likes

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -4,9 +4,11 @@ import React from 'react';
     return answers.map(answer => {
         return (
             <article className='answer' key={answer.id} id={answer.id}>
-                <p>{answer.answer_time}</p>
-                <button className='rating'>{answer.rating} likes</button>
-                <p>{answer.answer}</p>
+                <header class='answer-header'>
+                <p className="time">{answer.answer_time}</p>
+                <button className='like-btn'>{answer.rating} likes</button>
+                </header>
+                <p className="answer-block">{answer.answer}</p>
             </article>
         )     
     })
