a73c172ca422318a2bd84497905db2c885d7eac9
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-07-31 13:54:20 -0400

    Add Random Icon

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,6 +1,10 @@
 
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRandom } from '@fortawesome/free-solid-svg-icons'
+
+
 
 export const Question = ({ randomQuestion, postAnswer }) => {
     const [answer, setAnswer] = useState('')
@@ -20,7 +24,9 @@ export const Question = ({ randomQuestion, postAnswer }) => {
 
     return (
         <form className='question-form answerInput'>
-                <button className='shuffle-btn'> Shuffle Icon Goes Here</button>
+                <button className='shuffle-btn'>
+                <FontAwesomeIcon className='shuffle-icon' icon={faRandom} /> 
+                Shuffle Icon Goes Here</button>
             <header className="question-header">
                 <h2>{randomQuestion.question}</h2>
             </header>
