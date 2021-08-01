0f98af95ff08341dba5ce52949921f6b0437d938
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-01 14:50:06 -0600

    Remove property accessor from time

--- src/Utilities/util.js
+++ src/Utilities/util.js
@@ -1,6 +1,6 @@
 export const parseDate = (time) => {
   let a, b, date
-  a = time.answer_time.split('T');
+  a = time.split('T');
   b = a[0].split('-');
   date = b[1] + '-' + b[2] + '-' + b[0];
   return date
