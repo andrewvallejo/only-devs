eced8f19eefe1db6658bba8e74f012f6423b3d4e
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-08-01 19:42:43 -0400

    Rename file

---
+++ cypress/integration/main_page.js
@@ -0,0 +1,46 @@
+describe ('Prompt Question Page', () => {
+
+    const questionsReq = 'https://localhost:3001/api/questions'
+
+    beforeEach(() => {
+        cy.intercept('GET', questionsReq, {
+            statusCode: 200,
+            fixture: 'question_data'
+        })
+        cy.visit('http://localhost:3000/')
+    })
+    
+    it('should be able to visit the app and display the site', () => {
+        cy.contains('OnlyDevs')
+        cy.contains('View All Questions')
+        cy.get('.question-form')
+        cy.contains('Submit Answer')
+    })
+
+    it('should be able to navigate to all questions view', () => {
+        cy.get('.view-all-btn')
+        .click()
+        .url().should('include', '/all-questions')
+        cy.contains('Describe event bubbling.')
+    })
+
+    it('Should be able to be able to navigate back and forward in browser', () => {
+        cy.visit('http://localhost:3000/all-questions')
+          .go('back')
+          .url().should('eq','http://localhost:3000/')
+          .go('forward')
+          .url().should('eq','http://localhost:3000/all-questions')
+      })
+
+
+
+
+
+
+
+
+
+
+
+
+})
