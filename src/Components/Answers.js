e6ce4b225239cca6fff3a8aac1ae355c45733696
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-01 14:31:34 -0600

    Remove comments

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -3,11 +3,8 @@ import React from 'react';
  export const Answers = ({answers}) => {
     //  console.log
     return answers.map(answer => {
-        //Split at T
         let a = answer.answer_time.split('T');
-        //Split at dashes after taking the first index
         let b = a[0].split('-');
-        //Re-arrange and then join them with a dash.
         let date = b[1] + '-' + b[2] + '-' + b[0];
 
         return (
