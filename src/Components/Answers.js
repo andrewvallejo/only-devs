df7bdfb220a1cb25fe3cefe508d8b4d070240168
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-01 14:49:29 -0600

    Add destructing to each answer for legiability

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { parseDate } from  '../Utilities/util'
   
- export const Answers = ({answers}) => {
-    return answers.map(answer => {
-        const date = parseDate(answer)
-
+ export const Answers = ({answers, vote}) => {
+    return answers.map(singleAnswer => {
+        console.log(singleAnswer)
+        const {id, question_id, answer, rating, answer_time} = singleAnswer
+        const date = parseDate(answer_time)
         return (
-            <article className='answer' key={answer.id} id={answer.id}>
+            <article className='answer' key={id} id={id}>
                 <header class='answer-header'>
                 <p className="time">Submission date: {date}</p>
-                <button className='like-btn'>{answer.rating} likes</button>
+                <button className='like-btn' onClick={() => (vote(question_id, id ,'upvote' ))}>{rating} likes</button>
                 </header>
-                <p className="answer-block">{answer.answer}</p>
+                <p className="answer-block">{answer}</p>
             </article>
         )     
     })
