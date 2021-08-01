5f619745c811cae2df49e95c4cc1c9fb91a049d1
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-01 15:57:11 -0600

    Add vote features to up/down-vote buttons

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -3,14 +3,17 @@ import { parseDate } from  '../Utilities/util'
   
  export const Answers = ({answers, vote}) => {
     return answers.map(singleAnswer => {
-        console.log(singleAnswer)
         const {id, question_id, answer, rating, answer_time} = singleAnswer
         const date = parseDate(answer_time)
+        const upvote = [question_id, id, 'upvote']
+        const downvote = [question_id, id, 'downvote']
+        
+
         return (
             <article className='answer' key={id} id={id}>
                 <header className='answer-header'>
                 <p className="time">Submission date: {date}</p>
-                <button className='like-btn' onClick={() => (vote(question_id, id ,'upvote'))}>{rating} likes</button>
+                <button className='like-btn' onClick={() => (vote(upvote))}>{rating} likes</button>
                 </header>
                 <p className="answer-block">{answer}</p>
             </article>
