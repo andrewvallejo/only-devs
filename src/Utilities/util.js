26b087bf19cb93f0a479a6548fa7dfe85ba5b4a6
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-08-02 20:02:00 -0600

    Refactor - to be /

--- src/Utilities/util.js
+++ src/Utilities/util.js
@@ -2,6 +2,6 @@ export const parseDate = (time) => {
   let a, b, date
   a = time.split('T');
   b = a[0].split('-');
-  date = b[1] + '-' + b[2] + '-' + b[0];
+  date = b[1] + '/' + b[2] + '/' + b[0];
   return date
 }
