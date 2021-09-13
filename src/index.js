c25b534e7939c1d8ccb4e69b42da823b75db662c
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-12 22:19:08 -0600

    Add default import to App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import { App } from './Components/App';
 import './styles/styles.scss';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
