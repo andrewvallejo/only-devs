390b8f3754acb1fffc15402431e6448e4d0bc7cd
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-13 08:31:45 -0600

    Add reducer to set question in state

--- src/utility/reducer.js
+++ src/utility/reducer.js
@@ -1,3 +1,12 @@
-export const reducer = () => {
-
+export const reducer = (state, action) => {
+	const {type, value} = action
+	switch (type) {
+		case 'SETQUESTION':
+				return {
+					...state,
+					randomQuestion: value
+				}	
+		default:
+			break;
+	}
 }
