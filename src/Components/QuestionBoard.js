40fe65f4f6407901b00fbee7ba74dfa515372f72
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-13 19:15:24 -0600

    Remove unnecessary code to ready for refactor

--- src/Components/QuestionBoard.js
+++ src/Components/QuestionBoard.js
@@ -1,46 +1,41 @@
 import React from 'react';
 import { Card } from './Card';
-import {useState} from 'react';
-import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-export const QuestionBoard = ({questions}) => {
+export const QuestionBoard = ({ questions }) => {
     const [searchTerm, setSearchTerm] = useState('');
 
     const questionCard = questions.filter((question) => {
-        if(searchTerm === "") {
+        if (searchTerm === "") {
             return question;
-        } else if (question.question.toLowerCase().includes(searchTerm.toLowerCase())) {
+        }
+        if (question.question.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return question;
+        } else {
             return question;
         }
     }).map(question => {
         return (
-            <Card 
+            <Card
                 id={question.id}
                 key={question.id}
                 question={question.question}
             />
         )
     });
-    
+
     return (
         <>
             <input className='search-bar'
-                type="text" 
-                placeholder="Search Questions By Keyword..." 
+                type="text"
+                placeholder="Search Questions By Keyword..."
                 onChange={(event) => {
                     setSearchTerm(event.target.value);
-                }} 
+                }}
             />
             <section className='cards-container'>
-            {questionCard.length ? questionCard : <h2> There are no questions that match your search.</h2>} 
+                {questionCard.length ? questionCard : <h2> There are no questions that match your search.</h2>}
             </section>
         </>
     )
 }
-
-QuestionBoard.propTypes = {
-    questions: PropTypes.array,
-    searchTerm: PropTypes.string,
-  }
-
-
