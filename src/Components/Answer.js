9e003f7f65734cdd80b6356b5b470041b1903bcf
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-13 19:13:49 -0600

    Remove PropTypes

--- src/Components/Answer.js
+++ src/Components/Answer.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { parseDate } from  '../utility/util';
+import { parseDate } from '../utility/util';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronCircleUp, faChevronCircleDown  } from '@fortawesome/free-solid-svg-icons';
-import PropTypes from 'prop-types';
+import { faChevronCircleUp, faChevronCircleDown } from '@fortawesome/free-solid-svg-icons';
 
-export const Answer = ({singleAnswer, rate}) => {
+
+export const Answer = ({ singleAnswer, rate }) => {
 
   const [isDisabled, setIsDisabled] = useState(false);
 
-  const {id, question_id, answer , rating, answer_time} = singleAnswer
+  const { id, question_id, answer, rating, answer_time } = singleAnswer
   const date = parseDate(answer_time)
   const upvote = [question_id, id, 'upvote']
   const downvote = [question_id, id, 'downvote']
@@ -16,60 +16,55 @@ export const Answer = ({singleAnswer, rate}) => {
   let [count, setCount] = useState(rating)
 
   const addCount = (e) => {
-      e.preventDefault()
+    e.preventDefault()
 
-      setCount(count + 1)
-      rate(upvote) 
-      setIsDisabled(true);
+    setCount(count + 1)
+    rate(upvote)
+    setIsDisabled(true);
   }
 
   const removeCount = (e) => {
-      e.preventDefault()
+    e.preventDefault()
 
-      setCount(count - 1)
-      rate(downvote)
-      setIsDisabled(true);
+    setCount(count - 1)
+    rate(downvote)
+    setIsDisabled(true);
   }
 
-      return (
-          <article className='answer' key={id} id={id}>
-              <header className='answer-header'>
-                  <p className="time">Submission date: {date}</p>
-                  <div className="arrow-container">
-                  <button
-                    className={isDisabled ? 'disabled-vote' : ''}
-                    disabled={isDisabled}
-                    onClick={(e) => addCount(e)}
-                  >
-                    <FontAwesomeIcon
-                        icon={faChevronCircleUp}
-                    />
-                  </button>
-                  <h3 className='rating-number'>{count}</h3>
-                  <button
-                    className={isDisabled ? 'disabled-vote' : ''}
-                    disabled={isDisabled}
-                    onClick={(e) => removeCount(e)}
-                  >
-                    <FontAwesomeIcon 
-                        icon={faChevronCircleDown}
-                    />
-                </button>
-                </div>
-              </header>
-              <p> 
-                {isDisabled ? 'Recommendation recorded.': ''} 
-                <hr/>
-              </p>
-              <p 
-                className="answer-block">
-                {answer}  
-              </p>
-          </article>
-      )     
-}
-
-Answer.propTypes = {
-  singleAnswer: PropTypes.object,
-  rate: PropTypes.func
+  return (
+    <article className='answer' key={id} id={id}>
+      <header className='answer-header'>
+        <p className="time">Submission date: {date}</p>
+        <div className="arrow-container">
+          <button
+            className={isDisabled ? 'disabled-vote' : ''}
+            disabled={isDisabled}
+            onClick={(e) => addCount(e)}
+          >
+            <FontAwesomeIcon
+              icon={faChevronCircleUp}
+            />
+          </button>
+          <h3 className='rating-number'>{count}</h3>
+          <button
+            className={isDisabled ? 'disabled-vote' : ''}
+            disabled={isDisabled}
+            onClick={(e) => removeCount(e)}
+          >
+            <FontAwesomeIcon
+              icon={faChevronCircleDown}
+            />
+          </button>
+        </div>
+      </header>
+      <p>
+        {isDisabled ? 'Recommendation recorded.' : ''}
+        <hr />
+      </p>
+      <p
+        className="answer-block">
+        {answer}
+      </p>
+    </article>
+  )
 }
