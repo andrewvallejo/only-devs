2ca3b4663ed2b074e00d20027b44c0791191c55b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-13 19:16:13 -0600

    Refactor fetchQuestions to use axios

--- src/utility/apiCalls.js
+++ src/utility/apiCalls.js
@@ -1,54 +1,56 @@
-export const fetchQuestions = () => {
-  return fetch('https://onlydevs-api.herokuapp.com/questions')
-  .then(response => {
-    if (!response.ok) {
-      throw Error()
-    }
-    return response.json()
-  })
+import axios from "axios";
+
+export const fetchQuestions = async () => {
+  const config = {
+    method: 'get',
+    url: 'https://onlydevs-api.herokuapp.com/questions'
+  }
+  return await sendRequest(config)
 }
 
 export const fetchAnswers = (id) => {
   return fetch(`https://onlydevs-api.herokuapp.com/questions/${id}`)
-  .then(response => {
-    if(!response.ok) {
+    .then(response => {
+      if (!response.ok) {
         throw Error('Error fetching answers')
-    } 
-    return response.json()
-})
+      }
+      return response.json()
+    })
 }
 
 export const uploadAnswer = (data) => {
-  console.log("THIS IS JSONIFIED DATA", JSON.stringify(data));
   return fetch('https://onlydevs-api.herokuapp.com/questions/answer', {
     method: 'POST',
-    headers: {'content-type': 'application/json'},
+    headers: { 'content-type': 'application/json' },
     body: JSON.stringify(data)
   })
-  .then(response => {
-    if(!response.ok) {
-      throw Error('Error uploading answer')
-  } 
-    return response.json()
-})
-  .catch(err => console.error(err));
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Error uploading answer')
+      }
+      return response.json()
+    })
+    .catch(err => console.error(err));
 }
 
 export const postAnswerRating = (data) => {
-const [questionId, answerId, vote] =  data
- let answerData = {
+  const [questionId, answerId, vote] = data
+  let answerData = {
     "question_id": questionId,
     "answer_id": answerId,
     "vote": vote,
-}
+  }
   return fetch('https://onlydevs-api.herokuapp.com/questions/answer/vote', {
     method: 'POST',
-    headers: {'content-type': 'application/json'},
+    headers: { 'content-type': 'application/json' },
     body: JSON.stringify(answerData)
   })
-  .then(response => response.json())
-  .catch(err => console.error(err));
-} 
+    .then(response => response.json())
+    .catch(err => console.error(err));
+}
 
-
-
+const sendRequest = async(config) => {
+  return await axios(config)
+    .then(response => response.data)
+    .catch(error => console.error('Server Error: ' + error))
+}
