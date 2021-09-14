831953448ac7443ea207a40ec259da801c226c6e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-13 19:13:57 -0600

    Remove PropTypes

--- src/Components/AnswerBoard.js
+++ src/Components/AnswerBoard.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import { Answer } from './Answer';
-import PropTypes from 'prop-types';
-  
- export const AnswerBoard = ({answers, rateAnswer}) => {
+
+export const AnswerBoard = ({ answers, rateAnswer }) => {
     return answers.map(answer => {
         return (
             <Answer singleAnswer={answer} rate={rateAnswer} />
-    )
+        )
     })
-}
-
-
-AnswerBoard.propTypes = {
-    answers: PropTypes.array,
-    vote: PropTypes.string
 }
