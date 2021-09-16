22c1916d37a12120a0a1fbfebf91b38d458967fb
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-15 19:23:38 -0600

    Add HomePage component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { Question } from '../components/Question';
+import { QuestionBoard } from '../components/QuestionBoard';
+import { DevContext } from '../utility/DevContext';
+
+export const HomePage = () => {
+	const {state} = useContext(DevContext)
+	console.log()
+	return (
+
+			<>
+
+			<Question  question={state.question} />
+			<QuestionBoard questions={state.questions} />
+			</>
+    )
+}
