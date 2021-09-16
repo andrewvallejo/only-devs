3d8513e348f0ef4869a4efc6e6d52ac2b32aaf33
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 11:41:42 -0600

    Fix spacing and indentation

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,15 @@
-import React, { useContext } from 'react';
-import { Question } from '../components/Question';
-import { QuestionBoard } from '../components/QuestionBoard';
-import { DevContext } from '../utility/DevContext';
+import React, { useContext } from "react";
+import { Question } from "../components/Question";
+import { QuestionBoard } from "../components/QuestionBoard";
+import { DevContext } from "../utility/DevContext";
 
 export const HomePage = () => {
-	const {state} = useContext(DevContext)
-	console.log()
-	return (
-
-			<>
-
-			<Question  question={state.question} />
-			<QuestionBoard questions={state.questions} />
-			</>
-    )
-}
+  const { state } = useContext(DevContext);
+  console.log();
+  return (
+    <>
+      <Question question={state.question} />
+      <QuestionBoard questions={state.questions} />
+    </>
+  )
+};
