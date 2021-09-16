3acd87d2a8dd8c6ab2d58e5bc7f9cbf859693479
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 12:11:58 -0600

    Update file pathing to be cleaner

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Header = () => {
-  return (
-    <header className='main-header'>
-      <img className='logo' src='https://i.imgur.com/i6f9tQA.png' alt='onlyDevs logo' id='onlyDevsLogo' />
-    </header>
-  )
-}
