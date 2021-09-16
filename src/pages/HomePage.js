53327a97a27f2e4ca0ad185178e9608d046c01d1
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 12:29:22 -0600

    Update Question component and add Header

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,16 @@
 import React, { useContext } from "react";
-import { Question } from "../components/Question";
+import { QuestionForm } from "../components/QuestionForm";
 import { QuestionBoard } from "../components/QuestionBoard";
 import { DevContext } from "../utility/DevContext";
+import { Header } from "../components/Header";
 
 export const HomePage = () => {
   const { state } = useContext(DevContext);
   console.log();
   return (
     <>
-      <Question question={state.question} />
+      <Header />
+      <QuestionForm question={state.question} />
       <QuestionBoard questions={state.questions} />
     </>
   )
