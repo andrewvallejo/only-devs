4bbefb85490dcd50751f1ccefd20d0182822c457
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 12:22:27 -0600

    Add generic class name to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Card = ({ item }) => {
     return (
-        <article className='question-card'>
+        <article className='card question-card'>
             <h3>{item}</h3>
             <button className="view-details-btn">View Details</button>
         </article>
