a1d83cc082df5f08345c2df95a38b5fd8a717331
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 15:54:58 -0600

    Add event handler to goto full question page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
-export const Card = ({ item }) => {
+export const Card = ({ id, item }) => {
+    const history = useHistory()
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        history.push(`/questions/${id}`)
+    }
+
     return (
         <article className='card question-card'>
             <h3>{item}</h3>
-            <button className="view-details-btn">View Details</button>
+            <button className="view-details-btn"
+                onClick={(event) => handleClick(event)}
+            >View Details</button>
         </article>
     )
 }
