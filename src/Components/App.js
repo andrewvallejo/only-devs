d9c912e132eeae92d012223445801a599ae8bd06
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-15 19:22:47 -0600

    Refactor App to have clean Route paths

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useReducer } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Header } from './Header.js';
-import { Question } from './Question';
-import { QuestionBoard } from './QuestionBoard';
-import { fetchQuestions, uploadAnswer, postAnswerRating } from '../utility/apiCalls';
-import { DevContext } from '../utility/DevContext.js';
-import { reducer } from '../utility/reducer.js';
+import { HomePage } from '../pages/HomePage';
+import { QuestionPage } from '../pages/QuestionPage';
+import { fetchQuestions } from '../utility/apiCalls';
+import { DevContext } from '../utility/DevContext';
+import { reducer } from '../utility/reducer';
+import { randomize } from '../utility/util';
 
 const initialState = {
   question: [],
@@ -20,55 +20,48 @@ export const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
-    (async () => {
-      !state.questions.length &&
-        await fetchQuestions().then(data => {
-          dispatch({ state, action: { type: 'SETQUESTIONS', value: data } })
-          dispatch({ state, action: { type: 'SETQUESTION', value: randomize(data) } })
-        })
-    })()
+    (async () => !state.questions.length &&
+      await fetchQuestions().then(data => {
+        dispatch({ state, action: { type: 'SETQUESTIONS', value: data } })
+        dispatch({ state, action: { type: 'SETQUESTION', value: randomize(data) } })
+      })
+    )()
   }, [])
 
-  const postAnswer = (newAnswer) => {
-    uploadAnswer(newAnswer)
-      .then(response => {
-        if (!response) {
-          dispatch({ error: 'Oops,  our server is down! Your wasn\'t posted. Please try again later.' })
-          throw Error('Error fetching answers')
-        }
-        return response.json()
-      })
-      .catch(error => console.error(error))
-  }
-
-  const randomize = (items) => {
-    return items[Math.floor(Math.random() * items.length)]
-  }
-
-  const rateAnswer = (answer) => {
-    postAnswerRating(answer)
-      .then(response => { console.log(response) })
-  }
 
   return (
     <DevContext.Provider value={{ state, dispatch }}>
-      <Switch>
-        <Route exact path='/'>
-          <Header />
-          <main>
-            <Question question={state.question} postAnswer={postAnswer} />
-            <QuestionBoard vote={rateAnswer} questions={state.questions} />
-          </main>
-        </Route>
-      </Switch>
+      <main>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/question/:id' component={QuestionPage} />
+        </Switch>
+      </main>
     </DevContext.Provider>
   )
 };
 
-          // <Route exact path='/question-details/:id'>
-          //   <QuestionDetails
-          //       key={questionID}
-          //       id={questionID}
-          //       questions={state.questions}
-          //       rateAnswer={rateAnswer} /> 
-          // </Route>
+// <Route exact path='/question-details/:id'>
+//   <QuestionDetails
+//       key={questionID}
+//       id={questionID}
+//       questions={state.questions}
+//       rateAnswer={rateAnswer} /> 
+// </Route>
+
+// const postAnswer = (newAnswer) => {
+//   uploadAnswer(newAnswer)
+//     .then(response => {
+//       if (!response) {
+//         dispatch({ error: 'Oops,  our server is down! Your wasn\'t posted. Please try again later.' })
+//         throw Error('Error fetching answers')
+//       }
+//       return response.json()
+//     })
+//     .catch(error => console.error(error))
+// }
+
+// const rateAnswer = (answer) => {
+//   postAnswerRating(answer)
+//     .then(response => { console.log(response) })
+// }
