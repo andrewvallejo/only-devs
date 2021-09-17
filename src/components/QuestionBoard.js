e19ec7d2e9328245c3337f19d057ad318481a768
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-17 11:40:48 -0600

    Refactor to  pass dispatch and state

--- src/components/QuestionBoard.js
+++ src/components/QuestionBoard.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { useContext } from 'react';
-import { DevContext } from '../utility/DevContext';
 import { Card } from './Card';
 
-export const QuestionBoard = () => {
-  const { state: { questions } } = useContext(DevContext);
+export const QuestionBoard = ({ state, dispatch }) => {
 
-  const questionCard = questions.map(question => <Card key={question.id} id={question.id} question={question} />)
+  const questionCard = state.questions.map(question => {
+    return <Card key={question.id} state={state} dispatch={dispatch} question={question} />
+  })
 
   return (
-    <>
       <section className='cards-container'>
         {questionCard}
       </section>
-    </>
   )
 };
