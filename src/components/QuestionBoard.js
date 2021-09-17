d36ccd37d8d4b741499db069ff1dcdcc4774d0a7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 18:45:34 -0600

    Add global context only to page components

--- src/components/QuestionBoard.js
+++ src/components/QuestionBoard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useContext } from 'react';
+import { DevContext } from '../utility/DevContext';
 import { Card } from './Card';
 
-export const QuestionBoard = ({ questions }) => {
-   
-  const questionCard = questions.map(question => <Card key={question.id} id={question.id} item={question.question} />)
-  
+export const QuestionBoard = () => {
+  const { state: { questions } } = useContext(DevContext);
+
+  const questionCard = questions.map(question => <Card key={question.id} id={question.id} question={question} />)
+
   return (
-      <>
-        <section className='cards-container'>
-          {questionCard}
-        </section>
-      </>
-    )
+    <>
+      <section className='cards-container'>
+        {questionCard}
+      </section>
+    </>
+  )
 };
