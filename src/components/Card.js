fe26d0af50d2822ac233bbf3b04e893af5cc72e7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-17 12:18:57 -0600

    Add dispatch to load answers when question is click

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router';
+import { getAnswers } from '../utility/apiCalls';
+
 
 export const Card = ({ state, dispatch, question }) => {
   const history = useHistory()
 
-  const handleClick = (event) => {
-    event.preventDefault()
-    dispatch({ state, action: { type: 'SETQUESTION', value: question } })
+  const handleClick = () => {
+    getAnswers(question.id).then(allAnswers => {
+      dispatch({ state, action: { type: 'SETQUESTION', value: question } })
+      dispatch({ state, action: { type: 'SETANSWERS', value: allAnswers } })
+    })
     history.push(`/questions/${question.id}`)
   }
 
@@ -14,7 +18,7 @@ export const Card = ({ state, dispatch, question }) => {
     <article className='card question-card'>
       <h3>{question.question}</h3>
       <button className="view-details-btn"
-        onClick={(event) => handleClick(event)}>
+        onClick={handleClick}>
         View Details
       </button>
     </article>
