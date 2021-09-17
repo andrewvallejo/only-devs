4a5746002dce44224b2a689425931f04894cbe46
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-17 12:19:43 -0600

    Add answer board to be native to this component

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons';
-import { AnswerBoard } from './AnswerBoard';
 import { useHistory } from 'react-router';
-
+import { Answer } from './Answer';
 
 export const QuestionDetails = ({ state, dispatch }) => {
   const history = useHistory()
-
-  const onBack = (event) => {
-    history.goBack()
-  }
+  const answers = state.answers.map(answer => <Answer singleAnswer={answer} />)
 
   return (
     <section className='details-container'>
       <header className="question-header">
-        <button className='return-btn' onClick={(event) => onBack(event)}>
+        <button className='return-btn' onClick={() => history.goBack()}>
           <FontAwesomeIcon className='arrow-icon' icon={faAngleDoubleLeft} />
         </button>
         <h2 className="question">{state.selectedQuestion.question}</h2>
       </header>
       <div className="answers-board">
-      {/* <AnswerBoard /> */}
+        {answers}
       </div>
     </section>
   )
