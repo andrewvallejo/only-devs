00bbeb5ee9f82a7f3af6577a0fba0c8839cde071
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 18:44:00 -0600

    Update props

--- src/components/AnswerBoard.js
+++ src/components/AnswerBoard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Answer } from './Answer';
 
-export const AnswerBoard = ({ answers, rateAnswer }) => {
-    return answers.map(answer => <Answer singleAnswer={answer} rate={rateAnswer} />)   
+export const AnswerBoard = ({ answers }) => {
+    return answers.map(answer => <Answer singleAnswer={answer}/>)   
 }
