bb29dfed9dbc19f56b453361700db93854859926
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-17 12:18:41 -0600

    Remove redundnant component

--- src/components/AnswerBoard.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Answer } from './Answer';
-
-export const AnswerBoard = ({ answers }) => {
-    return answers.map(answer => <Answer singleAnswer={answer}/>)   
-}
