da8e4b6ccb3097b14fe43fd2f09c05c2b815dc61
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-17 11:40:16 -0600

    Refactor back button to use history

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -1,22 +1,27 @@
-import { AnswerBoard } from './AnswerBoard';
-import React, { useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons';
-import { useEffect } from 'react';
-import { getAnswers } from '../utility/apiCalls';
+import { AnswerBoard } from './AnswerBoard';
+import { useHistory } from 'react-router';
 
-export const QuestionDetails = ({ question }) => {
+
+export const QuestionDetails = ({ state, dispatch }) => {
+  const history = useHistory()
+
+  const onBack = (event) => {
+    history.goBack()
+  }
 
   return (
     <section className='details-container'>
       <header className="question-header">
-        <button className='return-btn'>
+        <button className='return-btn' onClick={(event) => onBack(event)}>
           <FontAwesomeIcon className='arrow-icon' icon={faAngleDoubleLeft} />
         </button>
-        <h2 className="question">{question.question}</h2>
+        <h2 className="question">{state.selectedQuestion.question}</h2>
       </header>
       <div className="answers-board">
-        {/* <AnswerBoard answers={answers} /> */}
+      {/* <AnswerBoard /> */}
       </div>
     </section>
   )
