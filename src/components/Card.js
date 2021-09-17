1a2cd29123b33860b32cc06b083595f72b11fb4e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-17 11:39:38 -0600

    Refactor state access by drilling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,22 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router';
-import { DevContext } from '../utility/DevContext';
 
-export const Card = ({ question }) => {
-    const { state, dispatch } = useContext(DevContext);
-    const history = useHistory()
-    const handleClick = (event) => {
-        event.preventDefault()
-        dispatch({ state, action: {type: 'SETQUESTION', value: question} })
-        history.push(`/questions/${question.id}`)
-    }
+export const Card = ({ state, dispatch, question }) => {
+  const history = useHistory()
 
-    return (
-        <article className='card question-card'>
-            <h3>{question.question}</h3>
-            <button className="view-details-btn"
-                onClick={(event) => handleClick(event)}>
-                View Details
-            </button>
-        </article>
-    )
+  const handleClick = (event) => {
+    event.preventDefault()
+    dispatch({ state, action: { type: 'SETQUESTION', value: question } })
+    history.push(`/questions/${question.id}`)
+  }
+
+  return (
+    <article className='card question-card'>
+      <h3>{question.question}</h3>
+      <button className="view-details-btn"
+        onClick={(event) => handleClick(event)}>
+        View Details
+      </button>
+    </article>
+  )
 }
