49e88b59c9cd8bd9a8c4b7832ee7e79f851e9b2e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 18:44:40 -0600

    Fix spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const Header = () => {
   const image = 'https://i.imgur.com/i6f9tQA.png';
+  
   return (
     <header className='main-header'>
       <img className='logo' src={image} alt='onlyDevs logo' />
