4bef4f8f10170169b3fba7aecfbdeb4c1c2d5f58
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-09-16 18:44:34 -0600

    Refactor card routing logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router';
+import { DevContext } from '../utility/DevContext';
 
-export const Card = ({ id, item }) => {
+export const Card = ({ question }) => {
+    const { state, dispatch } = useContext(DevContext);
     const history = useHistory()
-
     const handleClick = (event) => {
         event.preventDefault()
-        history.push(`/questions/${id}`)
+        dispatch({ state, action: {type: 'SETQUESTION', value: question} })
+        history.push(`/questions/${question.id}`)
     }
 
     return (
         <article className='card question-card'>
-            <h3>{item}</h3>
+            <h3>{question.question}</h3>
             <button className="view-details-btn"
                 onClick={(event) => handleClick(event)}>
                 View Details
