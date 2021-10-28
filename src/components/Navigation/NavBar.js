70cce47091b757ef5c18d6ce5052be736be0a817
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-10-28 16:34:38 -0600

    Rename header to NavBar

---
+++ src/components/Navigation/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PublicNav } from './NoAuthNav';
+
+export const NavBar = () => {
+  return (
+    <header className='NavBar'>
+      <nav ClassName='nav-container'>
+        {/* < PublicNav />  */}
+
+       </nav>
+    </header>
+  )
+};
