7d972ed2ac7a634c7ef7a097d2176a1b8c0b0449
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-10-28 16:33:49 -0600

    Add placeholder component  unauth navigation

---
+++ src/components/Navigation/NoAuthNav.js
@@ -0,0 +1,17 @@
+import { NavLink } from 'react-router-dom';
+
+export const PublicNav = () => {
+  return (
+    <nav ClassName='navigation public-nav'>
+      <ul className='navigation-links'>
+        <li>
+          <NavLink to='/connect'>Connect</NavLink>
+        </li>
+        <li>
+          <NavLink to='/register'>Sign Up</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
