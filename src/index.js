9b4beec6a306d3aac5072cd0f30b3c430cca6a6e
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-10-28 18:32:28 -0600

    Update App import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/App';
+import { App } from './App';
 import './styles/styles.scss';
 import { BrowserRouter } from 'react-router-dom';
 
