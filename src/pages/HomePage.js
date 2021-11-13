66e7c3f623864c98dd82421f4c57100c60da3f81
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-11-13 12:36:28 -0700

    Move app logic to homepage for Router purposes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react';
-import { QuestionForm } from '../components/QuestionForm';
+import React, { useContext, useEffect } from 'react';
+
 import { QuestionBoard } from '../components/QuestionBoard';
+import { QuestionForm } from '../components/QuestionForm';
+import { getQuestions } from '../utility/apiCalls';
 import { DevContext } from '../utility/DevContext';
+import { randomize } from '../utility/util';
+import { NavBar } from './NavBar';
 
 export const HomePage = () => {
   const { state, dispatch } = useContext(DevContext);
 
+  useEffect(() => {
+    (async () => !state.questions.length &&
+      await getQuestions().then(data => {
+        dispatch({ state, action: { type: 'SETQUESTIONS', value: data } });
+        dispatch({ state, action: { type: 'SETRANDOMQUESTION', value: randomize(data) } });
+      })
+    )();
+  }, [dispatch, state]);
+
   return (
-    <>
+    <main>
+      <NavBar />
       <QuestionForm question={state.randomQuestion} />
       <QuestionBoard state={state} dispatch={dispatch} />
-    </>
-  )
+    </main>
+  );
 };
+ 
