04624b7c0ea7989b3c05b1ddaf2451857309ba5d
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-11-13 12:30:01 -0700

    Replace useHIstory with useNavigate

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useHistory } from 'react-router';
+import { useNavigate } from 'react-router';
 import { getAnswers } from '../utility/apiCalls';
 
 
 export const Card = ({ state, dispatch, question }) => {
-  const history = useHistory()
+  const navigate = useNavigate()
 
   const handleClick = () => {
     getAnswers(question.id).then(allAnswers => {
       dispatch({ state, action: { type: 'SETQUESTION', value: question } })
       dispatch({ state, action: { type: 'SETANSWERS', value: allAnswers } })
     })
-    history.push(`/questions/${question.id}`)
+    navigate(`/questions/${question.id}`)
   }
 
   return (
