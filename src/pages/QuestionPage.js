5150a6892b900f7d5d20d585767697c8c80d92a9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-11-14 16:47:25 -0700

    Add components into question page

--- src/pages/QuestionPage.js
+++ src/pages/QuestionPage.js
@@ -1,13 +1,24 @@
 import React, { useContext } from 'react';
-import { Question } from '../components/Question'
+import { Header } from '../components/layout/Header';
+import { Question } from '../components/Question';
+import { QuestionForm } from '../components/QuestionForm';
+import { SideBar } from '../components/layout/SideBar';
 import { DevContext } from '../utility/DevContext';
 
 export const QuestionPage = () => {
+  const context = useContext(DevContext);
   const { state, dispatch } = useContext(DevContext);
 
   return (
-    <>
-      <Question state={state} dispatch={dispatch} />
-    </>
-  )
+    <body>
+      <Header />
+      <main className='question-page'>
+      <SideBar context={context} />
+        <Question state={state} dispatch={dispatch} />
+        <section className='main-area'>
+          <QuestionForm question={state.randomQuestion} />
+        </section>
+      </main>
+    </body>
+  );
 };
