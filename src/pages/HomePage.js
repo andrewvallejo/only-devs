cb9141b7f64c94474e2158bf54486d71fba71c31
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-11-14 17:34:29 -0700

    Fix spacing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,22 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 
-import { SideBar } from '../components/layout/SideBar';
-import { getQuestions } from '../utility/apiCalls';
-import { DevContext } from '../utility/DevContext';
-import { randomize } from '../utility/util';
 import { Header } from '../components/layout/Header';
+import { SideBar } from '../components/layout/SideBar';
+import { DevContext } from '../utility/DevContext';
+
 
 export const HomePage = () => {
   const context = useContext(DevContext);
-  const { state, dispatch } = context
-
-  useEffect(() => {
-    (async () => !state.questions.length &&
-      await getQuestions().then(data => {
-        dispatch({ state, action: { type: 'SETQUESTIONS', value: data } });
-        dispatch({ state, action: { type: 'SETRANDOMQUESTION', value: randomize(data) } });
-      })
-    )();
-  }, [dispatch, state]);
-
+  
   return (
-    <body>
+    <>
       <Header />
       <main className='home-page'>
-      <SideBar context={context} />
+        <SideBar context={context} />
+        <div className='some-content'>
+          <p>some content</p>
+        </div>
       </main>
-    </body>
+    </>
   );
 };
