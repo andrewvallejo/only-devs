32b04c99a8f2fa182ebbc3ab24143ecf9967e497
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-12-23 08:52:20 -0700

    Fix spacing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,19 @@
 import { NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
-  return (
-    <nav className="navigation public-nav">
-      <ul className="navigation-links">
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/connect">Connect</NavLink>
-        </li>
-        <li>
-          <NavLink to="/register">Sign Up</NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+	return (
+		<nav className='navigation public-nav'>
+			<ul className='navigation-links'>
+				<li>
+					<NavLink to='/'>Home</NavLink>
+				</li>
+				<li>
+					<NavLink to='/connect'>Connect</NavLink>
+				</li>
+				<li>
+					<NavLink to='/register'>Sign Up</NavLink>
+				</li>
+			</ul>
+		</nav>
+	)
 }
